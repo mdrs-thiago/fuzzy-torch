3f435cb534c83ff89e8712668e8569f96eac8f2a
Thiago Medeiros <thiago.medeiros.carvalho@gmail.com>
2026-01-26 14:49:57 -0300

    remove init py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .norm import TNorm, SNorm, Min, Max, Product, ProbabilisticSum, Lukasiewicz
-from .membership import MembershipFunction, TriangularMF, TrapezoidalMF, GaussianMF, BellMF, SigmoidMF
-from .variable import FuzzyVariable
-from .rule import FuzzyRule
-from .system import FuzzyModule
